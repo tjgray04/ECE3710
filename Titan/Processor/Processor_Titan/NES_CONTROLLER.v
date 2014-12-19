25c406ec3d38c40b8eda3397a0d40ed973fd5adb
tjgray04 <tjgray04@gmail.com>
2014-12-18 17:17:48 -0700

    commented NES CONTROLLER CODE

--- Titan/Processor/Processor_Titan/NES_CONTROLLER.v
+++ Titan/Processor/Processor_Titan/NES_CONTROLLER.v
@@ -19,9 +19,9 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module NES_CONTROLLER(
-		input clock, reset, data, //Input a clock , reset and data. The data line will be the output line containing the data from the controller.
-		output reg latch, pulse, 
-		output reg [7:0] controller_data
+		input clock, reset, data, //Input a clock (50 MHz, reset and data. The data line will be the output line containing the data from the controller.
+		output reg latch, pulse,  //Outputs used to shift the data out of the controller.
+		output reg [7:0] controller_data //Outputs the entire controller data that will hold what buttons if any were pressed.
     );
 
 	//Initialize the controller data
@@ -30,130 +30,97 @@ module NES_CONTROLLER(
 		controller_data = 8'b00000000;
 	end
 	
-	//Instantiate a register to conatin the count that will be send out all the control signals.
-//	reg [21:0] count; //was 31
+	//Instantiate a register to contain the count that will be send out all the control signals.
 	reg [20:0] count; //was 21
 	
 	/*This always block will take care the the synchronous portion of NES controller,
 	 *every 1/60 seconds a latch pulse will be sent, 18us later the pulse signal will 
-	 *toggle with every 6us. the negative edge of the pulse will be used to latch the data.
+	 *toggle every 6us. the time in which negative edge of the pulse will
+	 *be used to determine when to latch the data. These pulses are controlled by a counter that will determine when they will toggle.
 	 *
 	 */
 	always@(count)
 	begin
-//			if (count < 1200)
 			if (count < 600)
 			begin
 				latch <= 1'b1;
 				pulse <= 1'b1;
 			end
-			
-//			else if ((count >= 1200) && (count < 1800))
 			else if ((count >= 600) && (count < 900))
 			begin
-			latch <= 1'b0;	
-			pulse <= 1'b0;
+				latch <= 1'b0;	
+				pulse <= 1'b0;
 			end
-			
-//			else if ((count >= 1800) && (count < 2400))
 			else if ((count >= 900) && (count < 1200))
 			begin
-			latch <= 1'b0;
-         pulse <= 1'b1;
+				latch <= 1'b0;
+				pulse <= 1'b1;
 			end
-			
-//			else if ((count >= 2400) && (count < 3000))
 			else if ((count >= 1200) && (count < 1500))
 			begin	
-			latch <= 1'b0;
-			pulse <= 1'b0;
+				latch <= 1'b0;
+				pulse <= 1'b0;
 			end
-			
-//			else if ((count >= 3000) && (count < 3600))
 			else if ((count >= 1500) && (count < 1800))
 			begin
-			latch <= 1'b0;
-			pulse <= 1'b1;
+				latch <= 1'b0;
+				pulse <= 1'b1;
 			end
-			
-//			else if ((count >= 3600) && (count < 4200))
 			else if ((count >= 1800) && (count < 2100))
 			begin
-			latch <= 1'b0;
-			pulse <= 1'b0;
+				latch <= 1'b0;
+				pulse <= 1'b0;
 			end
-			
-//			else if ((count >= 4200) && (count < 4800))
 			else if ((count >= 2100) && (count < 2400))
 			begin
-			latch <= 1'b0;
-			pulse <= 1'b1;
+				latch <= 1'b0;
+				pulse <= 1'b1;
 			end
-			
-//			else if ((count >= 4800) && (count < 5400))
 			else if ((count >= 2400) && (count < 2700))
 			begin
-
-			latch <= 1'b0;
-			pulse <= 1'b0;
+				latch <= 1'b0;
+				pulse <= 1'b0;
 			end
-			
-//			else if ((count >= 5400) && (count < 6000))
 			else if ((count >= 2700) && (count < 3000))
 			begin
-			latch <= 1'b0;
-			pulse <= 1'b1;
+				latch <= 1'b0;
+				pulse <= 1'b1;
 			end
-			
-//			else if ((count >= 6000) && (count < 6600))
 			else if ((count >= 3000) && (count < 3300))
 			begin
-			latch <= 1'b0;
-			pulse <= 1'b0;	
+				latch <= 1'b0;
+				pulse <= 1'b0;	
 			end
-			
-//			else if ((count >= 6600) && (count < 7200))
 			else if ((count >= 3300) && (count < 3600))
 			begin
-			latch <= 1'b0;
-			pulse <= 1'b1;			
+				latch <= 1'b0;
+				pulse <= 1'b1;			
 			end
-			
-//			else if ((count >= 7200) && (count < 7800))
 			else if ((count >= 3600) && (count < 3900))
 			begin
-			latch <= 1'b0;
-			pulse <= 1'b0;
+				latch <= 1'b0;
+				pulse <= 1'b0;
 			end
-			
-//			else if ((count >= 7800) && (count < 8400))
 			else if ((count >= 3900) && (count < 4200))
 			begin
-			latch <= 1'b0;		
-			pulse <= 1'b1;	
+				latch <= 1'b0;		
+				pulse <= 1'b1;	
 			end
-			
-//			else if ((count >= 8400) && (count < 9000))
 			else if ((count >= 4200) && (count < 4500))
 			begin
-			latch <= 1'b0;
-			pulse <= 1'b0;
+				latch <= 1'b0;
+				pulse <= 1'b0;
 			end
-			
-//			else if ((count >= 9000) && (count < 9600))
 			else if ((count >= 4500) && (count < 4800))
 			begin
-			latch <= 1'b0;
-			pulse <= 1'b1;
+				latch <= 1'b0;
+				pulse <= 1'b1;
 			end
-			
-//			else if ((count >= 9600) && (count < 10200))
 			else if ((count >= 4800) && (count < 5100))
 			begin
-			latch <= 1'b0;
-			pulse <= 1'b0;
+				latch <= 1'b0;
+				pulse <= 1'b0;
 			end
-			
 			else
 			begin
 				latch <= 1'b0;
@@ -163,7 +130,7 @@ module NES_CONTROLLER(
 
 
 /*This always block will capture the data sent from the controller on the negative edge of the pulse, the controller by nature
- *is active low so the data will be inverted and stored into an output register
+ *is active low so the data will be inverted and stored into an output register.
  *
  */
 always@(posedge clock)
@@ -171,53 +138,42 @@ begin
 	if(reset)
 		controller_data = 8'b00000000;
 		//Capture A
-//   if ((count >= 1200) && (count < 1800))
-	else if (count == 600) //(count >= 599) && (count < 601))
+	else if (count == 600)
 		controller_data[7] = ~data;
 		//Capture B
-//	else if((count >= 2400) && (count < 3000))
-	else if(count == 1200)//(count >= 1199) && (count < 1201))
+	else if(count == 1200)
 		controller_data[6] = ~data;
-		//Caputre Select
-//	else if((count >= 3600) && (count < 4200))
-	else if(count == 1800)//(count >= 1799) && (count < 1801))
+	//Caputre Select
+	else if(count == 1800)
 		controller_data[5] = ~data;	
 	//Caputre Start
-//	else if((count >= 4800) && (count <5400))
-	else if(count == 2400)//(count >= 2399) && (count < 2401))
+	else if(count == 2400)
 		controller_data[4] = ~data;
 	//Caputre Up			
-//	else if((count >= 6000) && (count < 6600))
-	else if(count == 3000)//(count >= 2999) && (count < 3001))
+	else if(count == 3000)
 		controller_data[3] = ~data;
 	//Caputre Down
-//	else if((count >= 7200) && (count < 7800))
-	else if(count == 3600)//(count >= 3599) && (count < 3601))
+	else if(count == 3600)
 		controller_data[2] = ~data;	
 	//Caputre Left
-//	else if((count >= 8400) && (count < 9000))
-	else if(count == 4200)//(count >= 4199) && (count < 4201))
+	else if(count == 4200)
 		controller_data[1] = ~data;	
 	//Caputre Right
-//	else if((count >= 9600) &&(count < 10200))
-	else if(count == 4800)//(count >= 4799) && (count < 4801))
+	else if(count == 4800)
 		controller_data[0] = ~data;
 end
 
 
-/*This always block will take care of the counter that will be used to control.
+/*This always block will take care of the counter that will be used to control the module.
  *
  */
 always@(posedge clock)
 begin
 	//If a reset occurs reset the counter.
 	if(reset)
-//		count <= 21'd1200;
 		count <= 20'd0;
 	//Else if the counter has reached the maximum value, reset the counter
-//	else if(count == 21'd1666667)
 	else if(count == 20'd833333)
-//		count<= 21'b0;
 		count<= 20'b0;
 	//Else increase the counter.
 	else
